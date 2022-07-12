8ccf8e72e0d497e025d386c630899a00c6576bd7
Nhựt Linh <97160162+nhutlin231@users.noreply.github.com>
2022-07-13 02:52:06 +0700

    Delete tempCodeRunnerFile.cpp

--- Thuchanh/Buoi4/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-if(root != NULL) {
-        DuyetCayLNR(root->pLeft);
-        cout << root->value << '\t' << &(root->value) << '\t';
-        cout << "pLeft: " << &(root->pLeft->value) << '\t' << '\t';
-        cout << "pRight: " << &(root->pRight->value) << endl;
-        DuyetCayLNR(root->pRight);
-    }
